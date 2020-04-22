7651a858d39e89f169ee53dfadc105835e768294
Jesse Maxim <jessmaxim303@gmail.com>
2020-04-21 20:59:54 -0600

    Cleaned style conflict on header compo

--- src/About/About.js
+++ src/About/About.js
@@ -5,17 +5,33 @@ export const About = () => {
   return (
     <section className="about_container">
       <section className="about_info-container">
-        <h1>Hello.</h1>
-        <h1>My name is, Jesse Maxim.</h1>
-        <h1>SKILLS</h1>
-        <p>React</p>
+        <h1 className="about_info-title">Hello.</h1>
+        <h3 className="about_info-name">My name is, Jesse</h3>
+        <h1 className="about_info-skills">SKILLS</h1>
+        <p>React / React Native</p>
+        <p>Redux</p>
+        <p>JavaScript</p>
+        <p>jQuery</p>
+        <p>SCSS/CSS</p>
+        <p>RestAPI</p>
+        <p>Unit Testing</p>
+        <p>Git & Github</p>
       </section>
       <section className="about_introduction-container">
-    	  <h1>- introduction</h1>
+    	  <h1 className="about_info-skills">introduction</h1>
+        <h1 className="about_info-title">Creativity at the service of the user</h1>
     	  <p>Front-end software developer with an extensive background as a graphic designer. <br />
     	  Proficient in both front-end development languages like React, JavaScript, SCSS and, <br />
     	  also graphic design software including Adobe Creative Suite and inVision. Focused on <br />
     	  improving a user’s experience with a company or brand at all touchpoints.</p>
+        <button className="about_button">Fint out more</button>
+        <h1>Or find me @…</h1>
+        <div className="about_social-container">
+          <button>github</button>
+          <button>github</button>
+          <button>github</button>
+          <button>github</button>
+        </div>
       </section>	
     </section>
   	)
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -5,28 +5,11 @@ import './Projects.css';
 export const Projects = () => {
 	return (
     <section className="projects_container">
-      <section className="projects_into-container">- intro
-        <p>project details
-        </p>
+      <section className="projects_into-container">- projects
+        <p>I blend creativity and technical skills to improve the user experience and help 
+        <br />companies reach a larger, more engaged audience through the creation 
+        <br /> of modern websites designed to match a brand personality.</p>
       </section>
-      <section className="beer_project">
-        <h1 className="project_brand-title">the BEER REVIEW</h1>
-        <div className="project_color-box"></div>
-        <p className="project_info-para">An application to track feedback and reviews for almost every brewery in the country. 
-          <br /> The app fetches the data from an API and saves the users data locally using both React and Redux. 
-          <br /> Emphasizes testing to ensure to application behaves as expected.
-        </p>  
-        <button className="project_info-button">find out more</button>
-      </section>
-    	<section className="netflix_project">
-          <img className="netflix_logo" src={netflix}/>
-          <h3>2020 Redesign and feature install</h3>
-          <p>Netflix is a subscription-based film and television program rental service that offers media to subscribers via Internet streaming</p>
-          <button className="netflix_button">find out more</button>
-        </section>
-    	<section className="airbnb_project">Airbnb new feature
-          <button className="airbnb_button">find out more</button>
-        </section>
     </section>
 		)
 }
